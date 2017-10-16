680f09f34e187c2a74128f7358795faccbf3554e
ndean87 <ndean87@gmail.com>
2017-10-16 15:14:55 -0400

    just starting

--- js/init.js
+++ js/init.js
@@ -1,10 +1,16 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
-  var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
-  var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
+	var API_KEY = 'b5b642f523e3c4b4';
+	var URL =
+		'http://api.wunderground.com/api/' + API_KEY + '/hourly/q/NY/New_York.json';
+	var ctx = $('#NYCWeatherChart')
+		.get(0)
+		.getContext('2d');
 
-  makeAjaxRequest(URL, function(json) {
-    var data = generateDataSet(getHours(json), getFahrenheits(json));
-    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
-  });
+	// makeAjaxRequest(URL, function(json) {
+	// 	var data = generateDataSet(getHours(json), getFahrenheits(json));
+	// 	var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+	// });
+	makeAjaxRequest(URL, function(json) {
+		console.log(json);
+	});
 });
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,18 @@
-function getFahrenheits(result){
-  // Your code goes here
-}
-
-function getHours(result){
-  // Your code goes here
+function makeAjaxRequest(endpoint, success) {
+	fetch(endpoint)
+		.then(res => res.json())
+		.then(json => success(json));
+	// .then(success) is same as above;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+	// Your code goes here
 }
 
-function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+function getHours(result) {
+	// Your code goes here
+}
+
+function getFahrenheits(result) {
+	// Your code goes here
 }
